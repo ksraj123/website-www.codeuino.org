2b492a32e592e60d6a6dff40ef89428e0b85bbd4
ksraj123 <kumarsaurabhraj.sr@gmail.com>
2020-07-12 15:39:04 +0530

    twitt

--- src/pages/Home/NewHome.js
+++ src/pages/Home/NewHome.js
@@ -6,6 +6,7 @@ import LandingPageComponent from "./Components/LandingPageComponent";
 import Activities from "./Components/Activities";
 import Collaborate from "./Components/Collaborate";
 import Partners from "./Components/Partners";
+import Twitter from '../../components/Twitter/Twitter';
 import { TwitterTimelineEmbed } from "react-twitter-embed";
 const NewHome = () => {
 
@@ -38,8 +39,8 @@ const NewHome = () => {
             </span>
               <h1 className="mb-10 component-heading">Latest Updates</h1>
               <div className="row">
-                <div className="col-md-4">
-                  <TwitterTimelineEmbed
+                {/* <div className="col-md-4"> */}
+                  {/* <TwitterTimelineEmbed
                     sourceType="collection"
                     id="1281781304074203136"
                     options={{ height: 500, width: 500, fontSize:1 }}/>
@@ -56,7 +57,8 @@ const NewHome = () => {
                     sourceType="collection"
                     id="1281791360639500288"
                     options={{ height: 500, width: 500, fontSize:1 }}/>
-                </div>
+                </div> */}
+                <Twitter />
               </div>
             </div>
           </div>
